c951be7fc3b84e69d30c7b5e697dd929427201c0
Robert George <simion.robert.george@gmail.com>
2017-08-17 13:01:06 +0300

    Tryout

--- Erasmus-MTA/Erasmus-MTA/App_Start/BundleConfig.cs
+++ Erasmus-MTA/Erasmus-MTA/App_Start/BundleConfig.cs
@@ -14,18 +14,27 @@ namespace Erasmus_MTA
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jsgrid").Include(
+                      "~/Content/jsgrid.min.js",
+                      "~/Scripts/custom.jsgrid.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jstree").Include(
+                      "~/Scripts/dist/jstree.min.js",
+                      "~/Scripts/custom.jstree.js"));
+
+            bundles.Add(new StyleBundle("~/Content/jsgrid").Include(
+                      "~/Content/jsgrid.min.css",
+                      "~/Content/jsgrid-theme.min.css"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/sb-admin.css",
+                      "~/Scripts/dist/themes/proton/style.css"));
         }
     }
 }
---
+++ Erasmus-MTA/Erasmus-MTA/Scripts/custom.jstree.js
@@ -0,0 +1,178 @@
+﻿$(function () {
+
+    $('#jstree').jstree({
+        'plugins': ["wholerow", "checkbox"],
+        'core': {
+            'data': [{
+                "text": "Tara",
+                "state": {
+                    "opened": true
+                },
+                "children": [{
+                    "text": "Bulgaria",
+                    "state": {
+
+                        "selected": false
+                    }
+                }, {
+                    "text": "Croatia",
+                    "icon": "./assets/images/tree_icon.png"
+                }, {
+                    "text": "Grecia",
+                    "icon": "./assets/images/tree_icon.png"
+                },
+                {
+                    "text": "Croatia",
+                    "icon": "./assets/images/tree_icon.png"
+                }, {
+                    "text": "Grecia",
+                    "icon": "./assets/images/tree_icon.png"
+                },
+                {
+                    "text": "Croatia",
+                    "icon": "./assets/images/tree_icon.png"
+                }, {
+                    "text": "Grecia",
+                    "icon": "./assets/images/tree_icon.png"
+                },
+                {
+                    "text": "Croatia",
+                    "icon": "./assets/images/tree_icon.png"
+                }, {
+                    "text": "Grecia",
+                    "icon": "./assets/images/tree_icon.png"
+                },
+                {
+                    "text": "Croatia",
+                    "icon": "./assets/images/tree_icon.png"
+                }, {
+                    "text": "Grecia",
+                    "icon": "./assets/images/tree_icon.png"
+                }, {
+                    "text": "Croatia",
+                    "icon": "./assets/images/tree_icon.png"
+                }, {
+                    "text": "Grecia",
+                    "icon": "./assets/images/tree_icon.png"
+                }, {
+                    "text": "Croatia",
+                    "icon": "./assets/images/tree_icon.png"
+                }, {
+                    "text": "Grecia",
+                    "icon": "./assets/images/tree_icon.png"
+                }, {
+                    "text": "Croatia",
+                    "icon": "./assets/images/tree_icon.png"
+                }, {
+                    "text": "Grecia",
+                    "icon": "./assets/images/tree_icon.png"
+                }]
+            },
+            {
+                "text": "Oras",
+                "state": {
+                    "opened": true
+                },
+                "children": [{
+                    "text": "Bruxelles",
+                    "state": {
+                        "selected": false
+                    }
+                }, {
+                    "text": "Veliko Tarnovo",
+                    "icon": "./assets/images/tree_icon.png"
+                }, {
+                    "text": "Pardubice",
+                    "icon": "./assets/images/tree_icon.png"
+                }]
+            },
+            {
+                "text": "Departamet",
+                "state": {
+                    "opened": true
+                },
+                "children": [{
+                    "text": "Departamet1",
+                    "state": {
+                        "selected": false
+                    }
+                }, {
+                    "text": "Departamet2",
+                    "icon": "./assets/images/tree_icon.png"
+                }, {
+                    "text": "Departamet3",
+                    "icon": "./assets/images/tree_icon.png"
+                }]
+            },
+            {
+                "text": "Gen",
+                "state": {
+                    "opened": true
+                },
+                "children": [{
+                    "text": "Masculin",
+                    "state": {
+                        "selected": false
+                    }
+                }, {
+                    "text": "Feminin",
+                    "icon": "./assets/images/tree_icon.png"
+                }]
+            },
+            {
+                "text": "Partener",
+                "state": {
+                    "opened": true
+                },
+                "children": [{
+                    "text": "Partener1",
+                    "state": {
+                        "selected": false
+                    }
+                }, {
+                    "text": "Partener2",
+                    "icon": "./assets/images/tree_icon.png"
+                }]
+            },
+            {
+                "text": "Tip parteneri",
+                "state": {
+                    "opened": true
+                },
+                "children": [{
+                    "text": "Universitate militara",
+                    "state": {
+                        "selected": false
+                    }
+                }, {
+                    "text": "Universitate civila",
+                    "icon": "./assets/images/tree_icon.png"
+                }]
+            },
+            {
+                "text": "Nivel mobilitate",
+                "state": {
+                    "opened": true
+                },
+                "children": [{
+                    "text": "1",
+                    "state": {
+                        "selected": false
+                    }
+                }, {
+                    "text": "Partener2",
+                    "icon": "./assets/images/tree_icon.png"
+                }]
+            }
+            ],
+            'themes': {
+                'name': 'proton',
+                'responsive': true,
+                'icons': false
+            }
+
+        }
+    });
+
+
+});
