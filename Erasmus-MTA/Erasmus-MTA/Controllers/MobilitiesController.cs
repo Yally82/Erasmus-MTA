c368f1469dcf690d214354f1456d05addaf7cfe3
Robert George <simion.robert.george@gmail.com>
2017-09-08 20:26:30 +0300

    Actualizare JSTree

--- Erasmus-MTA/Erasmus-MTA/Controllers/MobilitiesController.cs
+++ Erasmus-MTA/Erasmus-MTA/Controllers/MobilitiesController.cs
@@ -35,36 +35,95 @@ namespace Erasmus_MTA.Controllers
             return View();
         }
 
-        //[HttpGet]
-        //public JsonResult getSelectParameters()
-        //{
+        [HttpGet]
+        public JsonResult getSelectParameters()
+        {
+            JsonData jsonData = new JsonData("Types");
 
-        //    aici apelam functiile
-        //    returnare tara / oras / departament / partenner / tip partener / an / nivel mobilitate / gen participant
+            foreach (Tara tara in database.Tara)
+            {
+                jsonData.tari.Add(new JstreeObject(tara.NumeEngleza));
+            }
+            foreach (DepartamenteATM dep in database.DepartamenteATM)
+            {
+                jsonData.departamente.Add(new JstreeObject(dep.Nume));
+            }
+            foreach(Oras oras in database.Oras)
+            {
+                jsonData.orase.Add(new JstreeObject(oras.Nume));
+            }
+            foreach( TipPartener tipPartener in database.TipPartener)
+            {
+                jsonData.tipPartener.Add(new JstreeObject(tipPartener.Denumire));
+            }
+           foreach( InstitutiiPartenere inst in database.InstitutiiPartenere)
+            {
+                jsonData.parteneri.Add(new JstreeObject(inst.Nume));
+            }
+            foreach (NivelStudii niv in database.NivelStudii)
+            {
+                jsonData.parteneri.Add(new JstreeObject(niv.Nivel));
+            }
 
-        //                  var jsonData = new
-        //                  {
-        //                      total = 1,
-        //                      page = 1,
-        //                      records = database.Employees.ToList().Count,
-        //                      rows = (
-        //                        from emp in database.Employees.ToList()
-        //                        select new
-        //                        {
-        //                            id = emp.id,
-        //                            cell = new string[] {
-        //              emp.Name.ToString(),
-        //              emp.Designation.ToString(),
-        //              emp.Gender.ToString(),
-        //              emp.Salary.ToString(),
-        //              emp.City, emp.State,
-        //              emp.Zip.ToString()
-        //                            }
-        //                        }).ToArray()
-        //                  };
-        //    return Json(jsonData, JsonRequestBehavior.AllowGet);
-        //}
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+        }
 
+        public class JsonData : JstreeObject
+        {
+            public JstreeObject tari = new JstreeObject("Tara");
+            public JstreeObject departamente= new JstreeObject("Departament");
+            public JstreeObject orase = new JstreeObject("Oras");
+            public JstreeObject parteneri = new JstreeObject("Partener");
+            public JstreeObject tipPartener = new JstreeObject("Tip Partener");
+            public JstreeObject niveleMobilitate = new JstreeObject("Nivel Mobilitate");
 
-    }
+            public JsonData(string name)
+                :base(name)
+            {
+                children = new List<JstreeObject>();
+                children.Add(tari);
+                children.Add(orase);
+                children.Add(departamente);
+                children.Add(parteneri);
+                children.Add(tipPartener);
+                children.Add(niveleMobilitate);
+
+                tari.children = new List<JstreeObject>();
+                departamente.children = new List<JstreeObject>();
+                orase.children = new List<JstreeObject>();
+                parteneri.children = new List<JstreeObject>();
+                tipPartener.children = new List<JstreeObject>();
+                niveleMobilitate.children = new List<JstreeObject>();
+            }
+        }
+
+        public class JstreeObject
+        {
+            public string text;
+            public int id;
+            public State state;
+            public List<JstreeObject> children;
+            public JstreeObject()
+            {
+                state = new State();
+                state.opened = true;
+                children = null;
+            }
+            public JstreeObject(string name)
+                :this()
+            { 
+                text = name;
+            }
+            public void Add(JstreeObject obj)
+            {
+                children.Add(obj);
+            }
+        }
+        public class State
+        {
+            public bool selected;
+            public bool opened;
+            public bool disabled;
+        }
+    };
 }
--- Erasmus-MTA/Erasmus-MTA/Scripts/jstree.mobilities.js
+++ Erasmus-MTA/Erasmus-MTA/Scripts/jstree.mobilities.js
@@ -1,178 +1,194 @@
 ﻿$(function () {
-
     $('#jstree').jstree({
-        'plugins': ["wholerow", "checkbox"],
         'core': {
-            'data': [{
-                "text": "Tara",
-                "state": {
-                    "opened": true
-                },
-                "children": [{
-                    "text": "Bulgaria",
-                    "state": {
-
-                        "selected": false
-                    }
-                }, {
-                    "text": "Croatia",
-                    "icon": "Content/images/tree_icon.png"
-                }, {
-                    "text": "Grecia",
-                    "icon": "Content/images/tree_icon.png"
-                },
-                {
-                    "text": "Croatia",
-                    "icon": "Content/images/tree_icon.png"
-                }, {
-                    "text": "Grecia",
-                    "icon": "Content/images/tree_icon.png"
-                },
-                {
-                    "text": "Croatia",
-                    "icon": "Content/images/tree_icon.png"
-                }, {
-                    "text": "Grecia",
-                    "icon": "Content/images/tree_icon.png"
-                },
-                {
-                    "text": "Croatia",
-                    "icon": "Content/images/tree_icon.png"
-                }, {
-                    "text": "Grecia",
-                    "icon": "Content/images/tree_icon.png"
-                },
-                {
-                    "text": "Croatia",
-                    "icon": "Content/images/tree_icon.png"
-                }, {
-                    "text": "Grecia",
-                    "icon": "Content/images/tree_icon.png"
-                }, {
-                    "text": "Croatia",
-                    "icon": "Content/images/tree_icon.png"
-                }, {
-                    "text": "Grecia",
-                    "icon": "Content/images/tree_icon.png"
-                }, {
-                    "text": "Croatia",
-                    "icon": "Content/images/tree_icon.png"
-                }, {
-                    "text": "Grecia",
-                    "icon": "Content/images/tree_icon.png"
-                }, {
-                    "text": "Croatia",
-                    "icon": "Content/images/tree_icon.png"
-                }, {
-                    "text": "Grecia",
-                    "icon": "Content/images/tree_icon.png"
-                }]
-            },
-            {
-                "text": "Oras",
-                "state": {
-                    "opened": true
-                },
-                "children": [{
-                    "text": "Bruxelles",
-                    "state": {
-                        "selected": false
-                    }
-                }, {
-                    "text": "Veliko Tarnovo",
-                    "icon": "Content/images/tree_icon.png"
-                }, {
-                    "text": "Pardubice",
-                    "icon": "Content/images/tree_icon.png"
-                }]
-            },
-            {
-                "text": "Departamet",
-                "state": {
-                    "opened": true
-                },
-                "children": [{
-                    "text": "Departamet1",
-                    "state": {
-                        "selected": false
-                    }
-                }, {
-                    "text": "Departamet2",
-                    "icon": "Content/images/tree_icon.png"
-                }, {
-                    "text": "Departamet3",
-                    "icon": "Content/images/tree_icon.png"
-                }]
-            },
-            {
-                "text": "Gen",
-                "state": {
-                    "opened": true
-                },
-                "children": [{
-                    "text": "Masculin",
-                    "state": {
-                        "selected": false
-                    }
-                }, {
-                    "text": "Feminin",
-                    "icon": "Content/images/tree_icon.png"
-                }]
-            },
-            {
-                "text": "Partener",
-                "state": {
-                    "opened": true
-                },
-                "children": [{
-                    "text": "Partener1",
-                    "state": {
-                        "selected": false
-                    }
-                }, {
-                    "text": "Partener2",
-                    "icon": "Content/images/tree_icon.png"
-                }]
-            },
-            {
-                "text": "Tip parteneri",
-                "state": {
-                    "opened": true
-                },
-                "children": [{
-                    "text": "Universitate militara",
-                    "state": {
-                        "selected": false
-                    }
-                }, {
-                    "text": "Universitate civila",
-                    "icon": "Content/images/tree_icon.png"
-                }]
-            },
-            {
-                "text": "Nivel mobilitate",
-                "state": {
-                    "opened": true
-                },
-                "children": [{
-                    "text": "1",
-                    "state": {
-                        "selected": false
-                    }
-                }, {
-                    "text": "Partener2",
-                    "icon": "Content/images/tree_icon.png"
-                }]
+            'data': {
+                "url": "Mobilities/Intern/GETJSON",
+                "dataType": "json" // needed only if you do not supply JSON headers
             }
-            ],
-            'themes': {
-                'name': 'proton',
-                'responsive': true,
-                'icons': false
-            }
-
         }
     });
-
-
 });
+
+
+
+
+
+
+//$(function () {
+
+//    $('#jstree').jstree({
+//        'plugins': ["wholerow", "checkbox"],
+//        'core': {
+//            'data': [{
+//                "text": "Tara",
+//                "state": {
+//                    "opened": true
+//                },
+//                "children": [{
+//                    "text": "Bulgaria",
+//                    "state": {
+
+//                        "selected": false
+//                    }
+//                }, {
+//                    "text": "Croatia",
+//                    "icon": "Content/images/tree_icon.png"
+//                }, {
+//                    "text": "Grecia",
+//                    "icon": "Content/images/tree_icon.png"
+//                },
+//                {
+//                    "text": "Croatia",
+//                    "icon": "Content/images/tree_icon.png"
+//                }, {
+//                    "text": "Grecia",
+//                    "icon": "Content/images/tree_icon.png"
+//                },
+//                {
+//                    "text": "Croatia",
+//                    "icon": "Content/images/tree_icon.png"
+//                }, {
+//                    "text": "Grecia",
+//                    "icon": "Content/images/tree_icon.png"
+//                },
+//                {
+//                    "text": "Croatia",
+//                    "icon": "Content/images/tree_icon.png"
+//                }, {
+//                    "text": "Grecia",
+//                    "icon": "Content/images/tree_icon.png"
+//                },
+//                {
+//                    "text": "Croatia",
+//                    "icon": "Content/images/tree_icon.png"
+//                }, {
+//                    "text": "Grecia",
+//                    "icon": "Content/images/tree_icon.png"
+//                }, {
+//                    "text": "Croatia",
+//                    "icon": "Content/images/tree_icon.png"
+//                }, {
+//                    "text": "Grecia",
+//                    "icon": "Content/images/tree_icon.png"
+//                }, {
+//                    "text": "Croatia",
+//                    "icon": "Content/images/tree_icon.png"
+//                }, {
+//                    "text": "Grecia",
+//                    "icon": "Content/images/tree_icon.png"
+//                }, {
+//                    "text": "Croatia",
+//                    "icon": "Content/images/tree_icon.png"
+//                }, {
+//                    "text": "Grecia",
+//                    "icon": "Content/images/tree_icon.png"
+//                }]
+//            },
+//            {
+//                "text": "Oras",
+//                "state": {
+//                    "opened": true
+//                },
+//                "children": [{
+//                    "text": "Bruxelles",
+//                    "state": {
+//                        "selected": false
+//                    }
+//                }, {
+//                    "text": "Veliko Tarnovo",
+//                    "icon": "Content/images/tree_icon.png"
+//                }, {
+//                    "text": "Pardubice",
+//                    "icon": "Content/images/tree_icon.png"
+//                }]
+//            },
+//            {
+//                "text": "Departamet",
+//                "state": {
+//                    "opened": true
+//                },
+//                "children": [{
+//                    "text": "Departamet1",
+//                    "state": {
+//                        "selected": false
+//                    }
+//                }, {
+//                    "text": "Departamet2",
+//                    "icon": "Content/images/tree_icon.png"
+//                }, {
+//                    "text": "Departamet3",
+//                    "icon": "Content/images/tree_icon.png"
+//                }]
+//            },
+//            {
+//                "text": "Gen",
+//                "state": {
+//                    "opened": true
+//                },
+//                "children": [{
+//                    "text": "Masculin",
+//                    "state": {
+//                        "selected": false
+//                    }
+//                }, {
+//                    "text": "Feminin",
+//                    "icon": "Content/images/tree_icon.png"
+//                }]
+//            },
+//            {
+//                "text": "Partener",
+//                "state": {
+//                    "opened": true
+//                },
+//                "children": [{
+//                    "text": "Partener1",
+//                    "state": {
+//                        "selected": false
+//                    }
+//                }, {
+//                    "text": "Partener2",
+//                    "icon": "Content/images/tree_icon.png"
+//                }]
+//            },
+//            {
+//                "text": "Tip parteneri",
+//                "state": {
+//                    "opened": true
+//                },
+//                "children": [{
+//                    "text": "Universitate militara",
+//                    "state": {
+//                        "selected": false
+//                    }
+//                }, {
+//                    "text": "Universitate civila",
+//                    "icon": "Content/images/tree_icon.png"
+//                }]
+//            },
+//            {
+//                "text": "Nivel mobilitate",
+//                "state": {
+//                    "opened": true
+//                },
+//                "children": [{
+//                    "text": "1",
+//                    "state": {
+//                        "selected": false
+//                    }
+//                }, {
+//                    "text": "Partener2",
+//                    "icon": "Content/images/tree_icon.png"
+//                }]
+//            }
+//            ],
+//            'themes': {
+//                'name': 'proton',
+//                'responsive': true,
+//                'icons': false
+//            }
+
+//        }
+//    });
+
+
+//});
